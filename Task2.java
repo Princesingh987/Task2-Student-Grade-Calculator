73e04c8b8bc6f66a596fc01690641019e45ae08c
Princesingh987 <168814139+Princesingh987@users.noreply.github.com>
2024-05-29 16:00:51 +0530

    Add files via upload

---
+++ Task2.java
@@ -0,0 +1,52 @@
+import java.util.Scanner;
+
+public class Task2 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.print("Enter the number of subjects: ");
+        int numSubjects = scanner.nextInt();
+        
+        int totalMarks = 0;
+        
+        for (int i = 1; i <= numSubjects; i++) {
+            System.out.print("Enter marks for Subject " + i + ": ");
+            int marks = scanner.nextInt();
+            
+       
+            if (marks < 0 || marks > 100) {
+                System.out.println("Invalid marks. Marks should be between 0 and 100.");
+                i--; 
+                continue;
+            }
+            
+            totalMarks += marks;
+        }
+        
+        
+        double averagePercentage = (double) totalMarks / numSubjects;
+        char grade = calculateGrade(averagePercentage);
+        
+      
+        System.out.println("Total Marks: " + totalMarks);
+        System.out.println("Average Percentage: " + averagePercentage);
+        System.out.println("Grade: " + grade);
+        
+        scanner.close();
+    }
+    
+
+    public static char calculateGrade(double averagePercentage) {
+        if (averagePercentage >= 90) {
+            return 'A';
+        } else if (averagePercentage >= 80) {
+            return 'B';
+        } else if (averagePercentage >= 70) {
+            return 'C';
+        } else if (averagePercentage >= 60) {
+            return 'D';
+        } else {
+            return 'F';
+        }
+    }
+}
